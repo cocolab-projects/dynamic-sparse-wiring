1b07af6102b66d00e619e4a6be0fc72234756ba9
Conner <connervercellino@gmail.com>
2019-08-01 16:13:06 -0700

    Adding WIP beam search implementation

--- modules/beam_search.py
+++ modules/beam_search.py
@@ -6,10 +6,13 @@ relaxation.
 from typing import Callable, Union
 
 import torch
+from torch.nn import functional as F
+
+from modules.subset_operator import subset_operator
 
 # A RoutingFunction takes a hidden_state as its first argument and one-hot
 # vectors representing the last decision (None if no prior decision)
-RoutingFunction = Callable[torch.Tensor, Union[torch.Tensor, None]]
+RoutingFunction = Callable
 
 
 def masking_topk(input_: torch.Tensor, k: int):
@@ -24,15 +27,9 @@ def masking_topk(input_: torch.Tensor, k: int):
     return torch.zeros_like(input_).scatter(1, indices, 1)
 
 
-def search_step(trajectory_scores: torch.Tensor) -> torch.Tensor:
-    '''
-    Takes a traversal step based on the current beam state.
-    '''
-    pass
-
-
 def beam_search(root_state: torch.Tensor, routing_function: RoutingFunction,
-                logits: int, beams: int, max_depth: int, temperature: float = 1.,
+                logits_size: int, beams: int, max_depth: int,
+                temperature: float = 1., preserve_zeros_grad: bool = True,
                 differentiable: bool = True):
     '''
     Implements a beam search with a (optional) top-k relaxation based on
@@ -55,12 +52,51 @@ def beam_search(root_state: torch.Tensor, routing_function: RoutingFunction,
     '''
 
     batch_size = root_state.size(0)
-    batches_logits_reshape = lambda tensor: tensor.view(-1 , beams * logits)
-    batches_beams_logits_reshape = lambda tensor: tensor.view(-1, beams, logits)
+    batches_logits_reshape = lambda tensor: tensor.view(batch_size, beams * logits_size)
+    batches_beams_logits_reshape = lambda tensor: tensor.view(-1, beams * logits_size)
 
     # stores past decisions
-    trajectories = torch.zeros(max_depth, batch_size,)
+    trajectories = torch.zeros(max_depth, batch_size, beams, logits_size)
     # score of each beam (unordered)
     trajectory_scores = torch.zeros(batch_size, beams, 1)
 
     hidden_state = root_state
+    last_decision = None
+
+    for depth in range(max_depth):
+        # current_state (batch size x beams x decisions)
+        hidden_state, logits = routing_function(hidden_state, last_decision)
+        log_probabilities = F.log_softmax(logits, dim=1)
+
+        # The "score" matrix has a shape (batch size x beams x logits) and holds
+        # all the scores we are locally selecting between
+        scores = (log_probabilities.unsqueeze(dim=1) +
+                  trajectory_scores.expand(-1, -1, logits_size))
+        scores = batches_logits_reshape(scores)
+        scores_mask, scores_indices = subset_operator(scores, k=beams,
+                                                      tau=temperature,
+                                                      hard=True)
+        score_values = scores_mask.gather(dim=1, index=scores_indices)
+        selected_decisions = torch.zeros(batch_size, beams, logits_size)
+
+        # create one-hot vectors for each one of the decisions
+        selected_decisions = selected_decisions.scatter(
+            2, scores_indices.unsqueeze(2), score_values.unsqueeze(2))
+
+        if preserve_zeros_grad:
+            # Preserve negative gradient for zeros
+            #         v copy gradient
+            # 0 1 1 0 0
+            # 1 0 1 0 0
+            #   ^ don't copy as a 1 exists in the column
+
+            # values expanded along the beam
+            expanded_values = score_values.unsqueeze(dim=1).expand(-1, beams, -1)
+            zeros_mask = ~expanded_values.byte()
+            selected_decisions[zeros_mask] = expanded_values[zeros_mask]
+
+        trajectory_scores += score_values
+        last_decision = batches_logits_reshape(selected_decisions)
+        trajectories[depth] = selected_decisions
+
+    return trajectories, trajectory_scores
