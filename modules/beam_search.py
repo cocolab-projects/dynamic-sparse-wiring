1ce735daa1bd133975289ed2e35d7ae739abcb72
Conner <connervercellino@gmail.com>
2019-08-01 10:23:32 -0700

    Scaffolding for stateless function (WIP)

---
+++ modules/beam_search.py
@@ -0,0 +1,54 @@
+'''
+Provides a simple implementation of a beam search with an optional top-k
+relaxation. 
+'''
+
+from typing import Callable, Union
+
+import torch
+
+# A RoutingFunction takes a hidden_state as its first argument and one-hot
+# vectors representing the last decision (None if no prior decisions)
+RoutingFunction = Callable[torch.Tensor, Union[torch.Tensor, None]]
+
+
+def search_step(trajectory_scores: torch.Tensor) -> torch.Tensor:
+    '''
+    Takes a traversal step based on the current beam state.
+    '''
+    pass
+
+
+def beam_search(root_state: torch.Tensor, routing_function: RoutingFunction,
+                logits: int, beams: int, max_depth: int, temperature: float 1.,
+                differentiable: bool = True):
+    '''
+    Implements a beam search with a (optional) top-k relaxation based on
+    [Reparameterizable Subset Sampling via Continuous Relaxations]
+    (https://arxiv.org/abs/1901.10517) and [Neural Nearest Neighbors Networks]
+    (https://arxiv.org/abs/1810.12575).
+
+    Args:
+        root_state: the inital hidden for the routing function, shared for all
+            children nodes
+        routing_function: a function that returns a new hidden_state along with
+            a logits over decisions
+        beams: the amount of paths to expand at each depth
+        temperature: used to "soften" the a softmax estimating argmax similar to
+            gumble-softmax
+        differentiable: use a relaxed top-k operator
+
+    Returns:
+        A sparse solution
+    '''
+
+    batch_size = root_state.size(0)
+    batches_logits_reshape = lambda tensor: tensor.view(-1 , beams * logits)
+    batches_beams_logits_reshape = lambda tensor: tensor.view(-1, beams, logits)
+
+    # stores past decisions
+    trajectories = torch.zeros(max_depth, batch_size,)
+    # score of each beam (unordered)
+    trajectory_scores = torch.zeros(batch_size, beams, 1)
+
+    hidden_state = root_state
