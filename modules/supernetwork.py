2b9525732b839dc5a1321e3ce370e312ee8a5554
Conner <connervercellino@gmail.com>
2019-08-05 17:58:06 -0700

    Working prototype for (unbatched) supernetwork

--- modules/supernetwork.py
+++ modules/supernetwork.py
@@ -1,5 +1,6 @@
 '''
 '''
+from typing import List
 
 import torch
 
@@ -13,6 +14,11 @@ from modules.beam_search import beam_search
 class Supernetwork(nn.Module):
 
     def __init__(self, beams: int, max_depth: int):
+        '''
+        Args:
+            beams
+            max_depth:
+        '''
         super().__init__()
         self.beams = beams
 
@@ -41,33 +47,39 @@ class Supernetwork(nn.Module):
         self.router = RNNRouter(128, self.logits_size)
         self.output_layer = nn.Linear(128, 10)
 
-    def forward(self, x):
+    def forward(self, x, tie_output_to_router: List[str] = ['pathwise']):
         batch_size = x.size(0)
         inital_hidden_state = self.net(x).view(batch_size, -1)
 
         # save copy for network
         rnn_hidden_state = inital_hidden_state[:]
-        path, _score = beam_search(rnn_hidden_state, self.router, logits_size=4,
+        result = beam_search(rnn_hidden_state, self.router, logits_size=4,
                                    beams=self.beams, max_depth=3)
         # path = (depth, batch_size, beams, logits)
         hidden_state = inital_hidden_state
         hidden_state = torch.unsqueeze(
             hidden_state, dim=1).expand(-1, self.beams, -1).clone().view(
-                self.beams * batch_size, self.logits_size)
+                self.beams * batch_size, -1)
 
-        for operations in path:
+        for operations in result.trajectories:
             # operations (batch_size, beams, logits) -> (batch_size * beams, logits)
             operations = operations.view(batch_size * self.beams,
                                          self.logits_size)
             outputs = []
             for index, each in enumerate(operations):
-                selected_operation_value = torch.max(each)
                 output = self.module_list[torch.argmax(each)](hidden_state[index])
-                output *= selected_operation_value
+                if 'operationwise' in tie_output_to_router:
+                    selected_operation_value = torch.max(each)
+                    output *= selected_operation_value
                 outputs.append(F.relu(output))
-            hidden_state = torch.cat(outputs, dim=1)
+            hidden_state = torch.stack(outputs, dim=0)
+        # returns batch_size * beams seems like this will need to change when we
+        # only run a few of the paths
+        predictions = self.output_layer(hidden_state)
+        if 'pathwise' in tie_output_to_router:
+            predictions *= torch.prod(result.score_values)
 
-        return self.output_layer(hidden_state)
+        return predictions
 
 
 if __name__ == '__main__':
