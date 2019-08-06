f3e524cb608d3827fb96ac3348f3e559bb850035
Conner <connervercellino@gmail.com>
2019-08-05 21:51:22 -0700

    Change data directory

--- simple_mnist.py
+++ simple_mnist.py
@@ -81,7 +81,7 @@ def main():
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('/Users/conner/datasets', train=True, download=True,
+        datasets.MNIST('./datasets', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.Resize(32),
                            transforms.ToTensor(),
@@ -89,7 +89,7 @@ def main():
                        ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('/Users/conner/datasets', train=False, transform=transforms.Compose([
+        datasets.MNIST('./datasets', train=False, transform=transforms.Compose([
                            transforms.Resize(32),
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
